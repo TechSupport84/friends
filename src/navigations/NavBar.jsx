3e11a2c6618e0de6f32fa2d0e6df8ef3bfe1ab6f
TechSupport84 <trainingtech84@gmail.com>
2024-11-10 23:43:42 +0300

    added

--- src/navigations/NavBar.jsx
+++ src/navigations/NavBar.jsx
@@ -4,51 +4,49 @@ import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
   const [activeIcon, setActiveIcon] = useState(null);
+  const navigate = useNavigate();
 
   const handleNavigation = (path, icon) => {
     navigate(path);
     setActiveIcon(icon);
   };
-  const navigate = useNavigate();
-
-
-  // Function to handle profile icon click
-   
 
   return (
-    <nav className="navbar">
+    <nav className="navbar ">
       <div className="navbar-left">
-        <a href="#" className="logo" onClick={() => navigate("/")} >ZedLuk</a>
+        <a href="#" className="logo" onClick={() => navigate("/")}>ZedLuk</a>
         <div className="navbar-center">
           <input type="text" placeholder="Search..." className="search-input" />
         </div>
       </div>
 
       <a href="#" className={`nav-link ${activeIcon === "home" ? "active" : ""}`}>
-        
-        <BiHomeAlt size={30} onClick={() =>  handleNavigation("/", "home")} /></a>
+        <BiHomeAlt size={30} onClick={() => handleNavigation("/", "home")} />
+      </a>
+      
       <a href="#" className={`nav-link ${activeIcon === "video" ? "active" : ""}`}>
-        <BiVideo size={30} onClick={() => handleNavigation("/video", "video")} /></a>
-      <a href="#" className={`nav-link ${activeIcon === "notification" ? "active" : ""}`}><BiBell size={30} onClick={() => handleNavigation("/notification", "notification")}/></a>
+        <BiVideo size={30} onClick={() => handleNavigation("/video", "video")} />
+      </a>
+      
+      <a href="#" className={`nav-link ${activeIcon === "notification" ? "active" : ""}`}>
+        <BiBell size={30} onClick={() => handleNavigation("/notification", "notification")} />
+      </a>
 
       <div className="navbar-right">
         <div className="icon-container">
-        
-          <a href="#"className={`nav-icon${activeIcon === "message" ? "active" : ""}`}>
-            <img src="../chat.png" alt="Chat" className="icon"  onClick={() =>  handleNavigation("/message", "message")}/>
-            </a>
+          <a href="#" className={`nav-icon ${activeIcon === "message" ? "active" : ""}`}>
+            <img src="../chat.png" alt="Chat" className="icon" onClick={() => handleNavigation("/message", "message")} />
+          </a>
 
-        <a href="#"className={`nav-icon${activeIcon === "menu" ? "active" : ""}`}>
-            <img src="../menu.png" alt="Menu" className="icon" onClick={() =>  handleNavigation("/settings", "menu")}/></a>
-          
-          <a href="#"className={`nav-icon${activeIcon === "profile" ? "active" : ""}`} >
-            <img src="../jeancy.jpg" alt="Profile" className="icon user-profile" onClick={() =>  handleNavigation("/profile", "profile")} />
-            
+          <a href="#" className={`nav-icon ${activeIcon === "menu" ? "active" : ""}`}>
+            <img src="../menu.png" alt="Menu" className="icon" onClick={() => handleNavigation("/settings", "menu")} />
           </a>
           
+          <a href="#" className={`nav-icon ${activeIcon === "profile" ? "active" : ""}`}>
+            <img src="../jeancy.jpg" alt="Profile" className="icon user-profile" onClick={() => handleNavigation("/profile", "profile")} />
+          </a>
         </div>
       </div>
-
     </nav>
   );
 };
